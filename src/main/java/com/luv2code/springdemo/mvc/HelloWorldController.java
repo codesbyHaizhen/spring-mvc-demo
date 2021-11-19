22f593f11c6dd86bad8949f0663ecb65619d4782
codesbyHaizhen <angelblanca1@hotmail.com>
2021-11-19 14:55:36 +0100

    read the form data with @RequestParam annotation

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -39,4 +40,20 @@ public class HelloWorldController {
 		
 		return "helloworld";
 	}
+	
+	// a new controller method to read form data with RequestParam annotation
+		@RequestMapping("/processFormVersionThree")
+		public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+			
+			// convert data in upper case
+			theName = theName.toUpperCase();
+			
+			// create the message
+			String result = "Hey, my Friend! " + theName;
+			
+			// add message to the model
+			model.addAttribute("message", result);
+			
+			return "helloworld";
+		}
 }
